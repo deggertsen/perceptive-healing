5bbb35cc41db3793da507c7bb6955c8865fa76b6
deggertsen <david@latitude.io>
2024-09-28 13:37:48 -0400

    Add protected route component and protect the user details screen

--- packages/app/features/user/detail-screen.tsx
+++ packages/app/features/user/detail-screen.tsx
@@ -1,5 +1,6 @@
 import { Button, Paragraph, YStack } from '@my/ui'
 import { ChevronLeft } from '@tamagui/lucide-icons'
+import { ProtectedRoute } from 'packages/app/provider/ProtectedRoute'
 import { useRouter } from 'solito/navigation'
 
 export function UserDetailScreen({ id }: { id: string }) {
@@ -8,11 +9,13 @@ export function UserDetailScreen({ id }: { id: string }) {
     return null
   }
   return (
-    <YStack f={1} jc="center" ai="center" gap="$4" bg="$background">
-      <Paragraph ta="center" fow="700" col="$blue10">{`User ID: ${id}`}</Paragraph>
-      <Button icon={ChevronLeft} onPress={() => router.back()}>
-        Go Home
-      </Button>
-    </YStack>
+    <ProtectedRoute>
+      <YStack f={1} jc="center" ai="center" gap="$4" bg="$background">
+        <Paragraph ta="center" fow="700" col="$blue10">{`User ID: ${id}`}</Paragraph>
+        <Button icon={ChevronLeft} onPress={() => router.back()}>
+          Go Home
+        </Button>
+      </YStack>
+    </ProtectedRoute>
   )
 }
